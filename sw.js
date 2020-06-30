f6d35c95037a220498eb733424157bb0af1269f7
jiangtj <jiangtj@users.noreply.github.com>
2020-06-30 06:59:41 +0000

    deploy: jiangtj/blog@61a00ee48dd1bb63a1229f5ba25565f825ae31da

--- sw.js
+++ sw.js
@@ -1,38 +1,22 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+/* global importScripts, workbox */
 
-importScripts(
-  "https://cdn.jsdelivr.net/npm/workbox-sw@4.3.1/build/workbox-sw.min.js"
-);
+'use strict';
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+importScripts('https://cdn.jsdelivr.net/npm/workbox-sw@5/build/workbox-sw.min.js');
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+const { NetworkFirst, StaleWhileRevalidate, CacheFirst } = workbox.strategies;
+const { registerRoute } = workbox.routing;
+const { ExpirationPlugin } = workbox.expiration;
+const { CacheableResponsePlugin } = workbox.cacheableResponse;
 
-workbox.routing.registerRoute(/\//, new workbox.strategies.NetworkFirst({ "cacheName":"index", plugins: [] }), 'GET');
-workbox.routing.registerRoute(/\.(?:js|css)$/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"js-css", plugins: [] }), 'GET');
-workbox.routing.registerRoute(/\.(?:png|gif|jpg|jpeg|svg)$/, new workbox.strategies.CacheFirst({ "cacheName":"images", plugins: [new workbox.expiration.Plugin({ maxEntries: 60, maxAgeSeconds: 2592000, purgeOnQuotaError: false })] }), 'GET');
-workbox.routing.registerRoute(/^https:\/\/fonts\.googleapis\.com/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"google-fonts-stylesheets", plugins: [] }), 'GET');
-workbox.routing.registerRoute(/^https:\/\/fonts\.gstatic\.com/, new workbox.strategies.CacheFirst({ "cacheName":"google-fonts-webfonts", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] }), new workbox.expiration.Plugin({ maxAgeSeconds: 31536000, purgeOnQuotaError: false })] }), 'GET');
 
-workbox.googleAnalytics.initialize({});
+
+registerRoute('/', new NetworkFirst({"cacheName":"index","plugins":[]}), 'GET');
+registerRoute(/\.(?:js|css)$/, new StaleWhileRevalidate({"cacheName":"js-css","plugins":[]}), 'GET');
+registerRoute(/\.(?:png|gif|jpg|jpeg|svg)$/, new CacheFirst({"cacheName":"images","plugins":[new ExpirationPlugin({"maxEntries":60,"maxAgeSeconds":2592000})]}), 'GET');
+registerRoute(/^https:\/\/fonts\.googleapis\.com/, new StaleWhileRevalidate({"cacheName":"google-fonts-stylesheets","plugins":[]}), 'GET');
+registerRoute(/^https:\/\/fonts\.gstatic\.com/, new CacheFirst({"cacheName":"google-fonts-webfonts","plugins":[new CacheableResponsePlugin({"statuses":[0,200]}),new ExpirationPlugin({"maxAgeSeconds":31536000})]}), 'GET');
+
+workbox.googleAnalytics.initialize();
+
+
